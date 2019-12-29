e3511643e0f64852857e944d91b13256884e1c78
Ragil <ragil@redpedia.com>
2019-12-29 14:14:27 +0700

    Pembelian Repo & controller by fahri

---
+++ Transaksi-PreOrder/Controller/PembelianController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using Transaksi_PreOrder.Model.Entity;
+using Transaksi_PreOrder.Model.Repository;
+using Transaksi_PreOrder.Model.Context;
+
+namespace Transaksi_PreOrder.Controller
+{
+    public class PembelianController
+    {
+        // CRUD
+        private PembelianRepository _repository;
+
+        public int Create(Pembelian pmb)
+        {
+            int result = 0;
+
+            // kode pembelian wajib diisi
+            if (string.IsNullOrEmpty(pmb.KdPembelian))
+            {
+                MessageBox.Show("Kode pembelian harus diisi !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // nama barang wajib diisi
+            if (string.IsNullOrEmpty(pmb.NamaBarang))
+            {
+                MessageBox.Show("Nama barang haru diisi !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // Harga pembelian wajib diisi
+            if (string.IsNullOrEmpty(Convert.ToString(pmb.HargaBeli)))
+            {
+                MessageBox.Show("Harga harus diisi !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // produsen
+            if (string.IsNullOrEmpty(pmb.KdProdusen))
+            {
+                MessageBox.Show("Kode produsen harus diisi !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // Barang
+            if (string.IsNullOrEmpty(pmb.KdBarang))
+            {
+                MessageBox.Show("Kode barang harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // Pesanan
+            if (string.IsNullOrEmpty(pmb.KdPesanan))
+            {
+                MessageBox.Show("Kode pesanan harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // Admin
+            if (string.IsNullOrEmpty(pmb.KdAdmin))
+            {
+                MessageBox.Show("Kode barang harus diisi !!!", "Peringatan",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            // membuat objek menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // buat clas repo
+                _repository = new PembelianRepository(context);
+
+                // manggil method repo biar nambah data
+                result = _repository.Create(pmb);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data pembelian berhasil disimpan !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Data pembelian gagal disimpan !!!", "Peringatan",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            return result;
+        }
+    }
+}
---
+++ Transaksi-PreOrder/Model/Repository/PembelianRepository.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+using Transaksi_PreOrder.Model.Entity;
+using Transaksi_PreOrder.Model.Context;
+
+namespace Transaksi_PreOrder.Model.Repository
+{
+    class PembelianRepository
+    {
+        private MySqlConnection _conn;
+
+        public PembelianRepository(DbContext context)
+        {
+            _conn = context.Conn;
+        }
+
+        public int Create(Pembelian pembelian)
+        {
+            int result = 0;
+
+            string sql = @"insert into pembelian (kd_pembelian, TglPembelian, NamaBarang, HargaBeli, kdProdusen, kdBarang, kdPesanan, kdAdmin) values
+        (@kd_pembelian, @TglPembelian, @NamaBarang, @HargaBeli, @kdProdusen, @kdBarang, @kdPesanan, @kdAdmin)";
+
+            using (MySqlCommand cmd = new MySqlCommand(sql, _conn))
+            {
+                cmd.Parameters.AddWithValue("@kd_pembelian", pembelian.KdPembelian);
+                cmd.Parameters.AddWithValue("@TglPembelian", pembelian.TglPembelian);
+                cmd.Parameters.AddWithValue("@NamaBarang", pembelian.NamaBarang);
+                cmd.Parameters.AddWithValue("@HargaBeli", pembelian.HargaBeli);
+                cmd.Parameters.AddWithValue("@kdProdusen", pembelian.KdProdusen);
+                cmd.Parameters.AddWithValue("@kdBarang", pembelian.KdBarang);
+                cmd.Parameters.AddWithValue("@kdPesanan", pembelian.KdPesanan);
+                cmd.Parameters.AddWithValue("@kdAdmin", pembelian.KdAdmin);
+
+                try
+                {
+                    result = cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.Print("Create error : {0}", ex.Message);
+                }
+            }
+
+            return result;
+        }
+
+        public List<Pembelian> ReadAll()
+        {
+            List<Pembelian> list = new List<Pembelian>();
+
+            try
+            {
+                string sql = @"select kd_pembelian, TglPembelian, Namabarang, HargaBeli, kdProdusen, kdBarang, kdPesanan, kdAdmin
+                from pembelian order by kd_pembelian";
+
+                using (MySqlCommand cmd = new MySqlCommand(sql, _conn))
+                {
+                    using (MySqlDataReader dtr = cmd.ExecuteReader())
+                    {
+                        while (dtr.Read())
+                        {
+                            Pembelian pmb = new Pembelian();
+
+                            pmb.KdPembelian = dtr["kd_pembelian"].ToString();
+                            pmb.TglPembelian = dtr["TglPembelian"].ToString();
+                            pmb.NamaBarang = dtr["NamaBarang"].ToString();
+                            pmb.HargaBeli = Convert.ToInt32(dtr["HargaBeli"].ToString());
+                            pmb.KdProdusen = dtr["kdProdusen"].ToString();
+                            pmb.KdBarang = dtr["kdBarang"].ToString();
+                            pmb.KdPesanan = dtr["kdPesanan"].ToString();
+                            pmb.KdAdmin = dtr["kdAdmin"].ToString();
+
+                            list.Add(pmb);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print("Read All error : {0}", ex.Message);
+            }
+            return list;
+        }
+
+    }
+}
